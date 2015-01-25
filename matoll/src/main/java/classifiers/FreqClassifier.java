90fb3100f1f1e584b236a22e0f0fcbfe39a504cb
Philipp Cimiano <cimiano@drfred.fritz.box>
2015-01-25 23:11:26 +0100

    added DummyFrequencyClassifier

---
+++ matoll/src/main/java/classifiers/FreqClassifier.java
@@ -0,0 +1,59 @@
+package classifiers;
+
+import java.util.Set;
+
+import sc.citec.uni.bielefeld.de.bimmel.learning.Classifier;
+import core.Dataset;
+import core.FeatureVector;
+
+public class FreqClassifier implements Classifier {
+
+	String Feature;
+	
+	double Threshold;
+	
+	public FreqClassifier(String feature, double threshold)
+	{
+		Feature = feature;
+		Threshold = threshold;
+	}
+	
+	public int predict(FeatureVector vector)
+	{
+		double value;
+		
+		value=  vector.getValueOfFeature(Feature);
+		
+		if (value >= Threshold)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	public void train(Dataset dataset) {
+		
+	}
+
+	public double predict(FeatureVector vector, int label) {
+		
+		double value;
+		
+		value=  vector.getValueOfFeature(Feature);
+		
+		if (value >= Threshold)
+		{
+			return value;
+		}
+		else
+		{
+			return 0;
+		}
+		
+	}
+
+
+}
