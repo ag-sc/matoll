551e8c18887d923d8abcee2b83a9b7cadcecfdbc
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2015-02-28 18:08:09 +0100

    minor changes

--- SentencePreprocessing/src/main/java/de/citec/sc/sentence/preprosessing/process/Process.java
+++ SentencePreprocessing/src/main/java/de/citec/sc/sentence/preprosessing/process/Process.java
@@ -20,10 +20,11 @@ public class Process {
 		 * examples for properties can be found under /src/test/resources/
 		 */
 		if (args.length == 0) {
-			System.out.println("Run: java Process propertylist");
+			System.out.println("Run: java Process ontology/pathToPropertyFile");
 			System.exit(1);
 		}
 		String endpoint = "http://dbpedia.org/sparql";
+		Boolean with_sentences = false;
 		/*
 		 *in pathToIndex only one index for one language can be found 
 		 */
@@ -34,7 +35,12 @@ public class Process {
 		IndexReader index = new IndexReader(pathToIndex);
 		List<List<String>> properties = new ArrayList<List<String>>();
 		try {
-			loadPropertyList(args[0],properties,language);
+			if(args[0].endsWith(".owl")){
+				loadOntology(args[0],properties,language);
+			}
+			else{
+				loadPropertyList(args[0],properties,language);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,43 +51,47 @@ public class Process {
 		System.out.println("Done");
 		System.out.println();
 		
-		String pathWriteSentences = folderToSaveResourcesSentences+"Sentences/";
-		if(checkFolder(pathWriteSentences)){
-			for (List<String> property: properties){
-				try {
-					String pathToSentenceModel = pathWriteSentences+property.get(1)+"/"+property.get(2)+"/"+property.get(3)+"/"+property.get(4);
-					boolean check_ontologyfolder = checkFolder(pathWriteSentences+property.get(1));
-					boolean check_namespacefolder = checkFolder(pathWriteSentences+property.get(1)+"/"+property.get(2));
-					boolean check_languagefolder = checkFolder(pathWriteSentences+property.get(1)+"/"+property.get(2)+"/"+property.get(3));
-					boolean check_modelfolder = checkFolder(pathToSentenceModel);
-					if(check_ontologyfolder&&check_languagefolder&&check_namespacefolder&&check_modelfolder){
-						System.out.println("Processing:"+property.get(0));
-						List<List<String>> entities = de.citec.sc.sentence.preprosessing.sparql.Resources.loadEntities(property, folderToSaveResourcesSentences);
-						List<List<String>> sentences = index.search(entities);
-						int value = 10000;
-						if(sentences.size()<=value){
-							de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences, pathToSentenceModel, language, property.get(0));
-						}
-						else{
-							int begin = 0;
-							int end = 0;
-							for(int i= 0; i<Math.floor((double)sentences.size()/value);i++){
-								begin = i*value;
-								end = begin+value;
-								de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences.subList(begin, end), pathToSentenceModel, language, property.get(0));
+		
+		if(with_sentences){
+			String pathWriteSentences = folderToSaveResourcesSentences+"Sentences/";
+			if(checkFolder(pathWriteSentences)){
+				for (List<String> property: properties){
+					try {
+						String pathToSentenceModel = pathWriteSentences+property.get(1)+"/"+property.get(2)+"/"+property.get(3)+"/"+property.get(4);
+						boolean check_ontologyfolder = checkFolder(pathWriteSentences+property.get(1));
+						boolean check_namespacefolder = checkFolder(pathWriteSentences+property.get(1)+"/"+property.get(2));
+						boolean check_languagefolder = checkFolder(pathWriteSentences+property.get(1)+"/"+property.get(2)+"/"+property.get(3));
+						boolean check_modelfolder = checkFolder(pathToSentenceModel);
+						if(check_ontologyfolder&&check_languagefolder&&check_namespacefolder&&check_modelfolder){
+							System.out.println("Processing:"+property.get(0));
+							List<List<String>> entities = de.citec.sc.sentence.preprosessing.sparql.Resources.loadEntities(property, folderToSaveResourcesSentences);
+							List<List<String>> sentences = index.search(entities);
+							int value = 10000;
+							if(sentences.size()<=value){
+								de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences, pathToSentenceModel, language, property.get(0));
 							}
-							de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences.subList(end, sentences.size()), pathToSentenceModel, language, property.get(0));
+							else{
+								int begin = 0;
+								int end = 0;
+								for(int i= 0; i<Math.floor((double)sentences.size()/value);i++){
+									begin = i*value;
+									end = begin+value;
+									de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences.subList(begin, end), pathToSentenceModel, language, property.get(0));
+								}
+								de.citec.sc.sentence.preprosessing.rdf.RDF.writeModel(sentences.subList(end, sentences.size()), pathToSentenceModel, language, property.get(0));
+							}
+							
+							System.out.println("Done");
+							System.out.println();
 						}
-						
-						System.out.println("Done");
-						System.out.println();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
 			}
 		}
+		
 		
 
 	}
@@ -100,6 +110,28 @@ public class Process {
 	    }
 	    
 	    for(String p: properties_raw.split("\n")){
+	    	String ontologyName =findOntologyName(p);
+	    	String[] tmp = p.split("/");
+	    	String name = tmp[tmp.length-1];
+	    	String namespace = tmp[tmp.length-2];
+	    	List<String> property = new ArrayList<String>();
+	    	property.add(p);
+	    	property.add(ontologyName);
+	    	property.add(namespace);
+	    	property.add(language);
+	    	property.add(name);
+	    	properties.add(property);
+	    	//System.out.println(property.toString());
+	    	
+	    }
+		
+	}
+	
+	private static void loadOntology(String pathToOntology,
+			List<List<String>> properties, String language) throws IOException {
+		OntologyImporter importer = new OntologyImporter(pathToOntology,"RDF/XML");
+	    
+	    for(String p: importer.getProperties()){
 	    	String ontologyName =findOntologyName(p);
 	    	String[] tmp = p.split("/");
 	    	String name = tmp[tmp.length-1];
