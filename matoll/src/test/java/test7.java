0d8178deda8e1163ef6d22c8a02dae95702bcab6
Philipp Cimiano <cimiano@DrFred.local>
2015-04-22 15:20:20 +0200

    added test7.java

---
+++ matoll/src/test/java/test7.java
@@ -0,0 +1,95 @@
+
+
+import de.citec.sc.bimmel.core.FeatureVector;
+import de.citec.sc.matoll.core.LexicalEntry;
+import de.citec.sc.matoll.core.Lexicon;
+import de.citec.sc.matoll.core.LexiconWithFeatures;
+import de.citec.sc.matoll.core.Sense;
+import de.citec.sc.matoll.core.SenseArgument;
+import de.citec.sc.matoll.core.SimpleReference;
+import de.citec.sc.matoll.core.SyntacticArgument;
+import de.citec.sc.matoll.core.SyntacticBehaviour;
+
+public class test7 {
+
+	public static void main(String[] args) {
+		// This test class checks if the equals method works...
+		
+		Lexicon lexicon = new Lexicon();
+		
+		LexicalEntry entry = new LexicalEntry();
+		
+		entry.setCanonicalForm("marry");
+		
+		Sense sense = new Sense();
+		
+		sense.setReference(new SimpleReference("http://dbpedia.org/ontology/spouse"));
+		
+		entry.setSense(sense);
+		
+		entry.setPOS("http://www.lexinfo.net/ontology/2.0/lexinfo#verb");
+		
+		SyntacticBehaviour behaviour = new SyntacticBehaviour();
+		
+		behaviour.setFrame("http://www.lexinfo.net/ontology/2.0/lexinfo#TransitiveFrame");
+				
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#subject","1",null));
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#directObject","2",null));
+		
+		sense.addSenseArg(new SenseArgument("http://lemon-model.net/lemon#subfOfProp","1"));
+		sense.addSenseArg(new SenseArgument("http://lemon-model.net/lemon#objOfProp","2"));
+		
+		lexicon.addEntry(entry);
+		
+		entry = new LexicalEntry();
+		
+		entry.setCanonicalForm("marry");
+		
+		sense = new Sense();
+		
+		sense.setReference(new SimpleReference("http://dbpedia.org/ontology/spouse"));
+		
+		entry.setSense(sense);
+		
+		entry.setPOS("http://www.lexinfo.net/ontology/2.0/lexinfo#verb");
+		
+		behaviour = new SyntacticBehaviour();
+		
+		behaviour.setFrame("http://www.lexinfo.net/ontology/2.0/lexinfo#TransitiveFrame");
+				
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#subject","1",null));
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#directObject","2",null));
+		
+		sense.addSenseArg(new SenseArgument("http://lemon-model.net/lemon#subfOfProp","1"));
+		sense.addSenseArg(new SenseArgument("http://lemon-model.net/lemon#objOfProp","2"));	
+		
+		entry.addSyntacticBehaviour(behaviour);
+		
+		behaviour = new SyntacticBehaviour();
+		
+		behaviour.setFrame("http://www.lexinfo.net/ontology/2.0/lexinfo#NounPPFrame");
+				
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#subject","1",null));
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#directObject","2",null));
+		
+		entry.addSyntacticBehaviour(behaviour);
+		
+		behaviour = new SyntacticBehaviour();
+		
+		behaviour.setFrame("http://www.lexinfo.net/ontology/2.0/lexinfo#IntransitivePP");
+				
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#subject","1",null));
+		behaviour.add(new SyntacticArgument("http://www.lexinfo.net/ontology/2.0/lexinfo#prepositionalObject","2","with"));
+		
+		entry.addSyntacticBehaviour(behaviour);
+		
+		
+		// The following should say that entry is already contained:
+		
+		if (lexicon.contains(entry)) System.out.println("Entry already included!!!");
+		else System.out.println("Entry not included!!!");
+		
+		
+	}
+
+}
