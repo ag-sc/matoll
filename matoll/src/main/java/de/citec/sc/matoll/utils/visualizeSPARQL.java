e43959f07c93bf963072d87de1618d4e951ad64a
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2015-07-30 17:23:35 +0200

    working on #61

--- matoll/src/main/java/de/citec/sc/matoll/utils/visualizeSPARQL.java
+++ matoll/src/main/java/de/citec/sc/matoll/utils/visualizeSPARQL.java
@@ -5,12 +5,47 @@
  */
 package de.citec.sc.matoll.utils;
 
+import de.citec.sc.matoll.patterns.SparqlPattern;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_1;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_2;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_3;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_4;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_5;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_6;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_7;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_8;
+import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_9;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_1;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_10;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_2;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_3;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_4;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_5;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_6;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_7;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_8;
+import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_9;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_1;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_2;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_2b;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_2c;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_3;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_4;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_5;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_6;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_7;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_7b;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_8;
+import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_9;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.apache.commons.lang3.StringUtils;
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
 
@@ -21,34 +56,65 @@ import org.jgrapht.graph.*;
 public class visualizeSPARQL {
     private static int variable_counter = 0;
     public static void main(String[] args){
-        String query = "SELECT ?lemma ?prefix ?e1_arg ?e2_arg ?prep WHERE"
-				+"{ "
-				+"{ ?y <conll:cpostag> \"NN\" . } "
-				+ " UNION "
-				+"{ ?y <conll:cpostag> \"NNS\" . } "
-				+ " UNION "
-				+"{ ?y <conll:cpostag> \"NNP\" . } "
-				+ "?y <conll:form> ?lemma . "
-				+"OPTIONAL{"
-				+ "?lemma_nn <conll:head> ?y. "
-				+ "?lemma_nn <conll:form> ?prefix. "
-				+ "?lemma_nn <conll:deprel> \"nn\"."
-				+"} "
-				+ "?verb <conll:head> ?y . "
-				+ "?verb <conll:deprel> \"cop\" ."
-				+ "?e1 <conll:head> ?y . "
-				+ "?e1 <conll:deprel> \"nsubj\" . "
-				+ "?p <conll:head> ?y . "
-				+ "?p <conll:deprel> \"prep\" . "
-				+ "?p <conll:form> ?prep . "
-				+ "?e2 <conll:head> ?p . "
-				+ "?e2 <conll:deprel> \"pobj\" . "
-				+ "?e1 <own:senseArg> ?e1_arg. "
-				+ "?e2 <own:senseArg> ?e2_arg. "
-				+ "}";
+        List<SparqlPattern> Patterns = new ArrayList<SparqlPattern>();
+        Patterns.add(new SparqlPattern_EN_1());
+        Patterns.add(new SparqlPattern_EN_2());
+        Patterns.add(new SparqlPattern_EN_3());
+        Patterns.add(new SparqlPattern_EN_4());
+        Patterns.add(new SparqlPattern_EN_5());
+        Patterns.add(new SparqlPattern_EN_6());
+        Patterns.add(new SparqlPattern_EN_7());
+        Patterns.add(new SparqlPattern_EN_8());
+        Patterns.add(new SparqlPattern_EN_9());
+        Patterns.add(new SparqlPattern_DE_1());
+        Patterns.add(new SparqlPattern_DE_2());
+        Patterns.add(new SparqlPattern_DE_3());
+        Patterns.add(new SparqlPattern_DE_4());
+//        Patterns.add(new SparqlPattern_DE_5());
+        Patterns.add(new SparqlPattern_DE_6());
+        Patterns.add(new SparqlPattern_DE_7());
+        Patterns.add(new SparqlPattern_DE_8());
+        Patterns.add(new SparqlPattern_DE_9());
+        Patterns.add(new SparqlPattern_DE_10());
+//        Patterns.add(new SparqlPattern_ES_1());
+//        Patterns.add(new SparqlPattern_ES_2());
+//        Patterns.add(new SparqlPattern_ES_2b());
+//        Patterns.add(new SparqlPattern_ES_2c());
+//        Patterns.add(new SparqlPattern_ES_3());
+//        Patterns.add(new SparqlPattern_ES_4());
+//        Patterns.add(new SparqlPattern_ES_5());
+//        Patterns.add(new SparqlPattern_ES_6());
+//        Patterns.add(new SparqlPattern_ES_7());
+//        Patterns.add(new SparqlPattern_ES_7b());
+//        Patterns.add(new SparqlPattern_ES_8());
+//        Patterns.add(new SparqlPattern_ES_9());
         
+        String prefix = "\\documentclass{scrartcl}\n" +
+        "\\usepackage{mathtools}\n" +
+        "\\usepackage{tikz}\n" +
+        "\\usetikzlibrary{trees,positioning}\n" +
+        "\n" +
+        "\\begin{document}\n";
+        
+        String suffix = "\\end{document}";
+        String output = "";
         System.out.println("Starting visualisation");
-        System.out.println(doVisual(query,"Patternx"));
+        for(SparqlPattern pattern : Patterns){
+            String tmp =doVisual(pattern.getQuery(),pattern.getID().replace("_","\\_"))+"\n\n\n";
+            if (tmp!=null)output+=tmp;
+            else System.out.println(pattern.getID()+" could not be visualized");
+        }
+            
+        
+        PrintWriter writer;
+        try {
+                writer = new PrintWriter("sparql_tree.tex");
+                writer.write(prefix+output+suffix);
+                writer.close();
+        } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+        }
         
     }
     
@@ -61,7 +127,7 @@ public class visualizeSPARQL {
         
         String suffix = "\\end{tikzpicture}\n"
                     +"\\caption{Visualisation for "+name+"}\n"
-                    +"\\label{fig:"+name+"}\n"
+                    +"\\label{fig:"+name.replace("\\_","")+"}\n"
                     +"\\end{figure}";
         
         /*
@@ -79,7 +145,7 @@ public class visualizeSPARQL {
             /*
             ignore everything which starts with optional or union (TODO: fix this)
             */
-            if(s.contains("<conll:head>")&& !s.contains("OPTIONAL") && !s.contains("UNION")) {
+            if(s.contains("<conll:head>")&& !s.contains("OPTIONAL") && !s.contains("UNION") &&!s.contains("SELECT")) {
                 String value = (s.replace("}", "")).trim();
                 String[] tmp = value.split("<conll:head>");
                 String subj = tmp[0].trim();
@@ -92,11 +158,16 @@ public class visualizeSPARQL {
                 relations.add(relation);
             }
             
-            if(s.contains("<conll:deprel>")&& !s.contains("OPTIONAL") && !s.contains("UNION")) {
+            if(s.contains("<conll:deprel>")&& !s.contains("OPTIONAL") && !s.contains("UNION") &&!s.contains("SELECT")) {
                 String value = (s.replace("}", "")).trim();
                 String[] tmp = value.split("<conll:deprel>");
                 String subj = tmp[0].trim();
                 String obj = tmp[1].trim();
+                /*
+                TODO: Be aware that sometimes the deprel is hiden in an additional variable.
+                In the moment ignore it, but make sure it is taken correctly
+                */
+                if(!obj.contains("?"))
                 deprel.put(subj, obj.replace("\"",""));
             }
         }
@@ -107,22 +178,38 @@ public class visualizeSPARQL {
         if(head_variable!=null){
             variables.put(head_variable,1);
             String latex_output = "\\node[fill = gray!40, "
-                    + "shape = rectangle, rounded corners,\n"
+                    + "shape = rectangle, rounded corners, "
                     +"minimum width = 2cm, font = \\sffamily] "
                     + "("+Integer.toString(variable_counter)+") {"+head_variable+"}\n";
             node_name.put(head_variable, Integer.toString(variable_counter));
+            int counter = 0;
             for(List<String> relation:relations){
                 String subj = relation.get(0);
                 if(!node_name.containsKey(subj)){
-                    latex_output+= "child {node[fill = blue!40, shape = rectangle, rounded corners,\n" 
+                    counter+=1;
+                    latex_output+= "child {node[fill = blue!40, shape = rectangle, rounded corners, " 
                             +"minimum width = 1cm, font = \\sffamily]  "
-                            + "("+Integer.toString(variable_counter+1)+") {"+subj+"}";
+                            + "("+Integer.toString(variable_counter+1)+") {"+subj+"}\n";
                     latex_output+=recursiveLoop(subj,relations,node_name);
                 }
-                
+            }
+//            //Set final number of}
+            int oc_1 = StringUtils.countMatches(latex_output+"};\n","}");
+            int oc_2 = StringUtils.countMatches(latex_output,"{");
+            if(oc_2>oc_1){
+                for(int i=0;i<oc_2-oc_1;i++)latex_output+="}";
+            }
+            String output = "";
+            if(oc_1>oc_2){
+                output = prefix+latex_output+";\n";
+            }
+            else{
+                output = prefix+latex_output+"};\n";
             }
             
-            String output = prefix+latex_output+"};\n";
+            
+            
+            
             
             output+="\\begin{scope}[nodes = {draw = none}]\n";
             for(List<String> relation:relations){
@@ -182,9 +269,9 @@ public class visualizeSPARQL {
             String obj = r.get(1);
             if(subj.equals(obj)){
                 String new_subj = r.get(0);
-                output+="child {node[fill = blue!40, shape = rectangle, rounded corners,\n" 
+                output+="child {node[fill = blue!40, shape = rectangle, rounded corners, " 
                       +"minimum width = 1cm, font = \\sffamily] "
-                      + "("+Integer.toString(variable_counter+1)+") {"+new_subj+"}";
+                      + "("+Integer.toString(variable_counter+1)+") {"+new_subj+"}\n";
                 go_on=true;
                 output+=recursiveLoop(new_subj,relations,node_name);
             }
