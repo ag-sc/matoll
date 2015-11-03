1033cafb1a0d2ce0ae872bc1099f7435dd30c42d
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2015-11-03 09:33:00 +0100

    worked on #15

--- matoll/src/test/java/CreateJoinedLexica.java
+++ matoll/src/test/java/CreateJoinedLexica.java
@@ -1,53 +1,26 @@
 
-import de.citec.sc.matoll.core.Language;
-import de.citec.sc.matoll.core.LexicalEntry;
+
 import de.citec.sc.matoll.core.Lexicon;
-import de.citec.sc.matoll.core.Provenance;
-import de.citec.sc.matoll.core.Reference;
 import de.citec.sc.matoll.io.LexiconLoader;
-import de.citec.sc.matoll.io.LexiconSerialization;
+
 import de.citec.sc.matoll.patterns.PatternLibrary;
 import de.citec.sc.matoll.patterns.SparqlPattern;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Intransitive_PP;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Noun_PP_appos;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Noun_PP_copulative;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Predicative_Participle_passive;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Transitive_Verb;
-import de.citec.sc.matoll.patterns.english.SparqlPattern_EN_Predicative_Participle_copulativ;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Predicative_Adjective;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Noun_PP;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Noun_Possessive;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Noun_Possessive_b;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Transitive;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Transitive_Passive;
-import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Transitive_Passive_optional;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Intransitive_PP;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Refelexive_Transitive_PP;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Noun_PP_appos;
 import de.citec.sc.matoll.patterns.german.SparqlPattern_DE_Noun_Possessive_appos;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Transitive_Verb;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Predicative_Participle_WithoutCopulative;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Noun_PP_copulative_b;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Noun_PP_copulative_withHop;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Noun_PP_copulative;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Noun_PP_appos_b;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Noun_PP_appos;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Predicative_Participle;
-import de.citec.sc.matoll.patterns.spanish.SPARQLPattern_ES_Intransitive_PP;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Reflexive_Transitive_withoutPrep;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Predicative_Participle_passive;
-import de.citec.sc.matoll.patterns.spanish.SparqlPattern_ES_Reflexive_Transitive_PP;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFFormat;
+
 
 /**
  *
