c0b35f705d6c8985872203c80f2720eef7b32712
Philipp Cimiano <cimiano@laverne.techfak.uni-bielefeld.de>
2015-11-06 09:30:39 +0100

    changed patterns

--- matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle.java
+++ matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle.java
@@ -5,7 +5,6 @@ import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,90 +15,78 @@ import de.citec.sc.matoll.core.Sentence;
 import de.citec.sc.matoll.patterns.SparqlPattern;
 import de.citec.sc.matoll.patterns.Templates;
 
-public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
+public class SparqlPattern_ES_Predicative_Participle_WithoutCopulative extends SparqlPattern{
 
-	Logger logger = LogManager.getLogger(SparqlPattern_ES_Predicative_Participle.class.getName());
+	Logger logger = LogManager.getLogger(SparqlPattern_ES_Predicative_Participle_WithoutCopulative.class.getName());
+	
 	
 
-	/*Neuer Parse:
-	 * 1	Su	su	d	DP3CS0	_	2	SPEC	_	_
-2	primo	primo	n	NCMS000	_	4	SUBJ	_	_
-3	Felipe_de_Suabia	felipe_de_suabia	n	NP00000	_	2	MOD	_	_
-4	estaba	estar	v	VAII1S0	_	0	ROOT	_	_
-5	casado	casar	v	VMP00SM	_	4	ATR	_	_
-6	con	con	s	SPS00	_	5	OBLC	_	_
-7	Irene_Angelina	irene_angelina	n	NP00000	_	6	COMP	_	_
+	/*
+1	La	el	d	DA0FS0	_	2	SPEC
+2	perla	perla	n	NCFS000	_	3	SUBJ
+3	es	ser	v	VSIP3S0	_	0	ROOT
+4	una	uno	d	DI0FS0	_	5	SPEC
+5	película	película	n	NCFS000	_	3	ATR
+6	mexicana	mexicano	a	AQ0FS0	_	5	MOD
+7	filmada	filmar	v	VMP00SF	_	5	MOD
+8	en	en	s	SPS00	_	7	MOD
+9	y	y	c	CC	_	8	COORD
+10	dirigida	dirigir	v	VMP00SF	_	9	CONJ
+11	por	por	s	SPS00	_	10	BYAG
+12	Emilio	emilio	n	NP00000	_	11	COMP
 
-// create 6b
- * 
- * 1	«	«	f	Fz	_	4	MOD
-2	Baby	baby	n	NP00000	_	1	MOD
-3	»	»	f	Fz	_	4	SUBJ
-4	fue	ser	v	VSIS3S0	_	0	ROOT
-5	escrita	escribir	v	VMP00SF	_	4	ATR
-6	por	por	s	SPS00	_	5	BYAG
-7	el	el	d	DA0MS0	_	8	SPEC
-8	cantante	cantante	n	NCCS000	_	6	COMP
-9	Justin_Bieber	justin_bieber	n	NP00000	_	8	MOD
-10	con	con	s	SPS00	_	8	MOD
-11	"	"	f	Fe	_	10	COMP
-12	Christopher	christopher	n	NP00000	_	8	MOD
-        
-        
+    TODO: "COMP" in ?prep can also be "MOD"
 
-// create 6c
- * 
- * 3	Beyoncé	beyoncé	n	NP00000	_	4	MOD
-14	había	haber	v	VAII1S0	_	15	AUX
-15	escrito	escribir	v	VMP00SM	_	12	COMP
-16	para	para	s	SPS00	_	15	MOD
-17	"	"	f	Fe	_	16	COMP
-18	Survivor	survivor	n	NP00000	_	15	DO
-
-	 * 
 	 */
         @Override
         public String getQuery() {
-            String query= "SELECT ?lemma ?e1_arg ?e2_arg ?prep WHERE {"
+            String query = "SELECT ?lemma ?e1_arg ?e2_arg ?prep  WHERE {"
 
-                            + "?e1 <conll:head> ?estar ."
-                            + "?e1 <conll:deprel> \"SUBJ\"."
+                    + "?copula <conll:postag> ?pos . "
+                    // can be VSII1S0 or "v"
+                    + "FILTER regex(?pos, \"VSI\") ."
+                    + "?copula <conll:lemma> \"ser\" ."
 
-                            + "?estar <conll:lemma> \"estar\" ."
-                            + "?estar <conll:postag> ?pos ."
-                            + "FILTER regex(?pos, \"VAII\") ."
+                    + "?noun <conll:head> ?copula . "
+                    + "?noun <conll:cpostag> \"n\" . "
+                    + "?noun <conll:deprel> \"ATR\" ."
 
-                            + "?participle <conll:lemma> ?lemma ."
-                            + "?participle <conll:head> ?estar ."
-                            + "?participle <conll:deprel> \"ATR\". "
-
-                            + "?p <conll:lemma> ?prep ."
-                            + "?p <conll:head> ?participle ."
-                            + "?p <conll:deprel> \"OBLC\" ."
+                    + "?participle <conll:form> ?lemma . "
+                    + "?participle <conll:head> ?noun . "
+                    + "?participle <conll:postag> ?participle_pos . "
+                    + "FILTER regex(?participle_pos, \"VMP\") ."
+                    + "?participle <conll:deprel> \"MOD\" ."
 
 
-                            + "?e2 <conll:head> ?p ."
-                            + "{?e2 <conll:deprel> \"COMP\". } UNION "
-                            + "{?e2 <conll:deprel> \"MOD\". }"
-                            //oder MOD?
+                    + "?p <conll:postag> ?prep_pos ."
+                    + "FILTER regex(?prep_pos, \"SPS\") ."
+                    + "?p <conll:head> ?participle . "
+                    + "?p <conll:lemma> ?prep . "
 
-                            + "?e1 <own:senseArg> ?e1_arg. "
-                            + "?e2 <own:senseArg> ?e2_arg. "
-                            + "}";
+                    + "?subj <conll:head> ?copula . "
+                    + "?subj <conll:deprel> \"SUBJ\" . "
+
+                    + "?pobj <conll:head> ?p . "
+                    + "?pobj <conll:deprel> \"COMP\" . "
+
+                    + "?subj <own:senseArg> ?e1_arg. "
+                    + "?pobj <own:senseArg> ?e2_arg. "
+                    + "}";
             return query;
         }
-			
+                        
 	@Override
 	public String getID() {
-		return "SparqlPattern_ES_Predicative_Participle";
+		return "SparqlPattern_ES_Predicative_Participle_WithoutCopulative";
 	}
 
 	@Override
 	public void extractLexicalEntries(Model model, Lexicon lexicon) {
-				
+		
+		
 		QueryExecution qExec = QueryExecutionFactory.create(getQuery(), model) ;
                 ResultSet rs = qExec.execSelect() ;
-                String adjective = null;
+                String noun = null;
                 String e1_arg = null;
                 String e2_arg = null;
                 String preposition = null;
@@ -110,7 +97,7 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
 
 
                          try{
-                                 adjective = qs.get("?lemma").toString();
+                                 noun = qs.get("?lemma").toString();
                                  e1_arg = qs.get("?e1_arg").toString();
                                  e2_arg = qs.get("?e2_arg").toString();	
                                  preposition = qs.get("?prep").toString();	
@@ -125,10 +112,10 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
                 }
                 qExec.close() ;
     
-		if(adjective!=null && e1_arg!=null && e2_arg!=null && preposition!=null) {
+		if(noun!=null && e1_arg!=null && e2_arg!=null && preposition!=null) {
                     Sentence sentence = this.returnSentence(model);
-                    Templates.getAdjective(model, lexicon, sentence, adjective, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
-            }
+                    Templates.getAdjective(model, lexicon, sentence, noun, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
+            } 
 		
 	}
 
---
+++ matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle_Copulative.java
@@ -0,0 +1,135 @@
+package de.citec.sc.matoll.patterns.spanish;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.citec.sc.matoll.core.Language;
+import de.citec.sc.matoll.core.Lexicon;
+import de.citec.sc.matoll.core.Sentence;
+import de.citec.sc.matoll.patterns.SparqlPattern;
+import de.citec.sc.matoll.patterns.Templates;
+
+public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
+
+	Logger logger = LogManager.getLogger(SparqlPattern_ES_Predicative_Participle.class.getName());
+	
+
+	/*Neuer Parse:
+	 * 1	Su	su	d	DP3CS0	_	2	SPEC	_	_
+2	primo	primo	n	NCMS000	_	4	SUBJ	_	_
+3	Felipe_de_Suabia	felipe_de_suabia	n	NP00000	_	2	MOD	_	_
+4	estaba	estar	v	VAII1S0	_	0	ROOT	_	_
+5	casado	casar	v	VMP00SM	_	4	ATR	_	_
+6	con	con	s	SPS00	_	5	OBLC	_	_
+7	Irene_Angelina	irene_angelina	n	NP00000	_	6	COMP	_	_
+
+// create 6b
+ * 
+ * 1	«	«	f	Fz	_	4	MOD
+2	Baby	baby	n	NP00000	_	1	MOD
+3	»	»	f	Fz	_	4	SUBJ
+4	fue	ser	v	VSIS3S0	_	0	ROOT
+5	escrita	escribir	v	VMP00SF	_	4	ATR
+6	por	por	s	SPS00	_	5	BYAG
+7	el	el	d	DA0MS0	_	8	SPEC
+8	cantante	cantante	n	NCCS000	_	6	COMP
+9	Justin_Bieber	justin_bieber	n	NP00000	_	8	MOD
+10	con	con	s	SPS00	_	8	MOD
+11	"	"	f	Fe	_	10	COMP
+12	Christopher	christopher	n	NP00000	_	8	MOD
+        
+        
+
+// create 6c
+ * 
+ * 3	Beyoncé	beyoncé	n	NP00000	_	4	MOD
+14	había	haber	v	VAII1S0	_	15	AUX
+15	escrito	escribir	v	VMP00SM	_	12	COMP
+16	para	para	s	SPS00	_	15	MOD
+17	"	"	f	Fe	_	16	COMP
+18	Survivor	survivor	n	NP00000	_	15	DO
+
+	 * 
+	 */
+        @Override
+        public String getQuery() {
+            String query= "SELECT ?lemma ?e1_arg ?e2_arg ?prep WHERE {"
+
+                            + "?e1 <conll:head> ?estar ."
+                            + "?e1 <conll:deprel> \"SUBJ\"."
+
+                            + "?estar <conll:lemma> \"estar\" ."
+                            + "?estar <conll:postag> ?pos ."
+                            + "FILTER regex(?pos, \"VAII\") ."
+
+                            + "?participle <conll:lemma> ?lemma ."
+                            + "?participle <conll:head> ?estar ."
+                            + "?participle <conll:deprel> \"ATR\". "
+
+                            + "?p <conll:lemma> ?prep ."
+                            + "?p <conll:head> ?participle ."
+                            + "?p <conll:deprel> \"OBLC\" ."
+
+
+                            + "?e2 <conll:head> ?p ."
+                            + "{?e2 <conll:deprel> \"COMP\". } UNION "
+                            + "{?e2 <conll:deprel> \"MOD\". }"
+                            //oder MOD?
+
+                            + "?e1 <own:senseArg> ?e1_arg. "
+                            + "?e2 <own:senseArg> ?e2_arg. "
+                            + "}";
+            return query;
+        }
+			
+	@Override
+	public String getID() {
+		return "SparqlPattern_ES_Predicative_Participle";
+	}
+
+	@Override
+	public void extractLexicalEntries(Model model, Lexicon lexicon) {
+				
+		QueryExecution qExec = QueryExecutionFactory.create(getQuery(), model) ;
+                ResultSet rs = qExec.execSelect() ;
+                String adjective = null;
+                String e1_arg = null;
+                String e2_arg = null;
+                String preposition = null;
+
+                try {
+                 while ( rs.hasNext() ) {
+                         QuerySolution qs = rs.next();
+
+
+                         try{
+                                 adjective = qs.get("?lemma").toString();
+                                 e1_arg = qs.get("?e1_arg").toString();
+                                 e2_arg = qs.get("?e2_arg").toString();	
+                                 preposition = qs.get("?prep").toString();	
+                          }
+	        	 catch(Exception e){
+	     	    	e.printStackTrace();
+                        }
+                     }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                qExec.close() ;
+    
+		if(adjective!=null && e1_arg!=null && e2_arg!=null && preposition!=null) {
+                    Sentence sentence = this.returnSentence(model);
+                    Templates.getAdjective(model, lexicon, sentence, adjective, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
+            }
+		
+	}
+
+}
