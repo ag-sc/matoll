3373f507d50956d38d24350edefc2e4f012eace2
Philipp Cimiano <cimiano@laverne.techfak.uni-bielefeld.de>
2015-11-06 09:42:03 +0100

    new Spanish patterns

--- matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle_Copulative.java
+++ matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle_Copulative.java
@@ -21,67 +21,239 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
 	Logger logger = LogManager.getLogger(SparqlPattern_ES_Predicative_Participle.class.getName());
 	
 
-	/*Neuer Parse:
-	 * 1	Su	su	d	DP3CS0	_	2	SPEC	_	_
-2	primo	primo	n	NCMS000	_	4	SUBJ	_	_
-3	Felipe_de_Suabia	felipe_de_suabia	n	NP00000	_	2	MOD	_	_
-4	estaba	estar	v	VAII1S0	_	0	ROOT	_	_
-5	casado	casar	v	VMP00SM	_	4	ATR	_	_
-6	con	con	s	SPS00	_	5	OBLC	_	_
-7	Irene_Angelina	irene_angelina	n	NP00000	_	6	COMP	_	_
+//	D:555
+//	property subject: El Estado y la revolución
+//	property subject uri: http://dbpedia.org/resource/The_State_and_Revolution
+//	property object: Lenin
+//	property object uri: http://dbpedia.org/resource/Vladimir_Lenin
+//	sentence:: 
+//	1	El	el	d	DA0MS0	_	2	SPEC	_	_
+//	2	Estado	estado	n	NP00000	_	6	SUBJ	_	_
+//	3	y	y	c	CC	_	2	COORD	_	_
+//	4	la	el	d	DA0FS0	_	5	SPEC	_	_
+//	5	revolución	revolución	n	NCFS000	_	3	CONJ	_	_
+//	6	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	7	un	uno	d	DI0MS0	_	8	SPEC	_	_
+//	8	libro	libro	n	NCMS000	_	6	ATR	_	_
+//	9	escrito	escribir	v	VMP00SM	_	8	MOD	_	_
+//	10	por	por	s	SPS00	_	9	BYAG	_	_
+//	11	Lenin	lenin	n	NP00000	_	10	COMP	_	_
+//	12	entre	entre	s	SPS00	_	9	MOD	_	_
+//	13	agosto	[??:??/8/??:??.??:??]	w	W	_	12	COMP	_	_
+//	14	y	y	c	CC	_	13	COORD	_	_
+//	15	septiembre_de_1917	[??:??/9/1917:??.??:??]	w	W	_	14	CONJ	_	_
+//	16	,	,	f	Fc	_	15	punct	_	_
+//	17	mientras	mientras	c	CS	_	6	MOD	_	_
+//	18	se	se	p	P00CN000	_	19	MOD	_	_
+//	19	encontraba	encontrar	v	VMII3S0	_	17	COMP	_	_
+//	20	en	en	s	SPS00	_	19	PP-LOC	_	_
+//	21	la	el	d	DA0FS0	_	22	SPEC	_	_
+//	22	clandestinidad	clandestinidad	n	NCFS000	_	20	COMP	_	_
+//	23	en	en	s	SPS00	_	22	MOD	_	_
+//	24	Finlandia	finlandia	n	NP00000	_	23	COMP	_	_
+//	25	.	.	f	Fp	_	24	punct	_	_
+//
+//	ID:5
+//	property subject: La vida secreta de las plantas
+//	property subject uri: http://dbpedia.org/resource/The_Secret_Life_of_Plants
+//	property object: La vida secreta de las plantas
+//	property object uri: http://dbpedia.org/resource/The_Secret_Life_of_Plants
+//	sentence:: 
+//	1	La	el	d	DA0FS0	_	2	SPEC	_	_
+//	2	vida	vida	n	NCFS000	_	8	SUBJ	_	_
+//	3	secreta	secreto	a	AQ0FS0	_	2	MOD	_	_
+//	4	de	de	s	SPS00	_	2	COMP	_	_
+//	5	las	el	d	DA0FP0	_	6	SPEC	_	_
+//	6	plantas	planta	n	NCFP000	_	4	COMP	_	_
+//	7	,	,	f	Fc	_	6	punct	_	_
+//	8	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	9	un	uno	d	DI0MS0	_	10	SPEC	_	_
+//	10	libro	libro	n	NCMS000	_	8	ATR	_	_
+//	11	escrito	escribir	v	VMP00SM	_	10	MOD	_	_
+//	12	por	por	s	SPS00	_	11	BYAG	_	_
+//	13	Peter_Tompkins	peter_tompkins	n	NP00000	_	12	COMP	_	_
+//	14	y	y	c	CC	_	13	COORD	_	_
+//	15	Christopher_Bird	christopher_bird	n	NP00000	_	14	CONJ	_	_
+//	16	.	.	f	Fp	_	15	punct	_	_
+//	
+//	ID:133
+//	property subject: Adenet Le Roi
+//	property subject uri: http://dbpedia.org/resource/Adenes_Le_Roi
+//	property object: 1240
+//	property object uri: 1240-01-01^^http://www.w3.org/2001/XMLSchema#gYear
+//	sentence:: 
+//	1	Adenet	adenet	n	NP00000	_	3	SUBJ	_	_
+//	2	le	le	p	PP3CSD00	_	3	IO	_	_
+//	3	Roi	roi	n	NP00000	_	4	SUBJ	_	_
+//	4	fue	ser	v	VSIS3S0	_	0	ROOT	_	_
+//	5	un	uno	d	DI0MS0	_	6	SPEC	_	_
+//	6	juglar	juglar	n	NCMS000	_	4	ATR	_	_
+//	7	nacido	nacer	v	VMP00SM	_	6	MOD	_	_
+//	8	hacia	hacia	s	SPS00	_	7	MOD	_	_
+//	9	1240	1240	z	Z	_	8	COMP	_	_
+//	10	y	y	c	CC	_	9	COORD	_	_
+//	11	muerto	morir	v	VMP00SM	_	10	CONJ	_	_
+//	12	hacia	hacia	s	SPS00	_	11	MOD	_	_
+//	13	1300	1300	z	Z	_	12	COMP	_	_
+//	14	.	.	f	Fp	_	13	punct	_	_
+//	
 
-// create 6b
- * 
- * 1	«	«	f	Fz	_	4	MOD
-2	Baby	baby	n	NP00000	_	1	MOD
-3	»	»	f	Fz	_	4	SUBJ
-4	fue	ser	v	VSIS3S0	_	0	ROOT
-5	escrita	escribir	v	VMP00SF	_	4	ATR
-6	por	por	s	SPS00	_	5	BYAG
-7	el	el	d	DA0MS0	_	8	SPEC
-8	cantante	cantante	n	NCCS000	_	6	COMP
-9	Justin_Bieber	justin_bieber	n	NP00000	_	8	MOD
-10	con	con	s	SPS00	_	8	MOD
-11	"	"	f	Fe	_	10	COMP
-12	Christopher	christopher	n	NP00000	_	8	MOD
-        
-        
+//	ID:38
+//	property subject: Antrim
+//	property subject uri: http://dbpedia.org/resource/Antrim,_County_Antrim
+//	property object: Condado de Antrim
+//	property object uri: http://dbpedia.org/resource/County_Antrim
+//	sentence:: 
+//	1	El	el	d	DA0MS0	_	4	SPEC	_	_
+//	2	Carrick-a-rede	carrick-a-rede	n	NP00000	_	5	SUBJ	_	_
+//	3	rope	rope	n	NCMS000	_	2	COMP	_	_
+//	4	bridge	bridge	n	NCMS000	_	3	COMP	_	_
+//	5	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	6	un	uno	d	DI0MS0	_	7	SPEC	_	_
+//	7	puente	puente	n	NCMS000	_	5	ATR	_	_
+//	8	colgante	colgante	a	AQ0CS0	_	7	MOD	_	_
+//	9	situado	situar	v	VMP00SM	_	7	MOD	_	_
+//	10	en_las_cercanías_de	en_las_cercanías_de	s	SPS00	_	9	MOD	_	_
+//	11	Ballintoy	ballintoy	n	NP00000	_	10	COMP	_	_
+//	12	,	,	f	Fc	_	11	punct	_	_
+//	13	condado	condado	n	NCMS000	_	11	MOD	_	_
+//	14	de	de	s	SPS00	_	13	MOD	_	_
+//	15	Antrim	antrim	n	NP00000	_	14	COMP	_	_
+//	16	,	,	f	Fc	_	15	punct	_	_
+//	17	Irlanda_de_el_Norte	irlanda_de_el_norte	n	NP00000	_	15	MOD	_	_
+//	18	.	.	f	Fp	_	17	punct	_	_
+//
+//	ID:8
+//	property subject: Dart
+//	property subject uri: http://dbpedia.org/resource/Dart_(programming_language)
+//	property object: Google
+//	property object uri: http://dbpedia.org/resource/Google
+//	sentence:: 
+//	1	DartEditor	darteditor	n	NP00000	_	2	SUBJ	_	_
+//	2	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	3	el	el	d	DA0MS0	_	5	SPEC	_	_
+//	4	primer	1	a	AO0MS0	_	5	MOD	_	_
+//	5	editor	editor	n	NCMS000	_	2	ATR	_	_
+//	6	lanzado	lanzar	v	VMP00SM	_	5	MOD	_	_
+//	7	por	por	s	SPS00	_	6	BYAG	_	_
+//	8	Google	google	n	NP00000	_	7	COMP	_	_
+//	9	(	(	f	Fpa	_	10	punct	_	_
+//	10	noviembre_de_2011	[??:??/11/2011:??.??:??]	w	W	_	6	MOD	_	_
+//	11	)	)	f	Fpt	_	10	punct	_	_
+//	12	para	para	s	SPS00	_	5	MOD	_	_
+//	13	escribir	escribir	v	VMN0000	_	12	COMP	_	_
+//	14	aplicaciones	aplicación	n	NCFP000	_	13	DO	_	_
+//	15	Dart	dart	n	NP00000	_	14	MOD	_	_
+//	16	.	.	f	Fp	_	15	punct	_	_
 
-// create 6c
- * 
- * 3	Beyoncé	beyoncé	n	NP00000	_	4	MOD
-14	había	haber	v	VAII1S0	_	15	AUX
-15	escrito	escribir	v	VMP00SM	_	12	COMP
-16	para	para	s	SPS00	_	15	MOD
-17	"	"	f	Fe	_	16	COMP
-18	Survivor	survivor	n	NP00000	_	15	DO
+//	ID:37
+//	property subject: BlackBerry OS
+//	property subject uri: http://dbpedia.org/resource/BlackBerry_OS
+//	property object: BlackBerry
+//	property object uri: http://dbpedia.org/resource/BlackBerry_Limited
+//	sentence:: 
+//	1	El	el	d	DA0MS0	_	2	SPEC	_	_
+//	2	BlackBerry_Q10	blackberry_q10	n	NP00000	_	3	SUBJ	_	_
+//	3	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	4	un	uno	d	DI0MS0	_	5	SPEC	_	_
+//	5	teléfono	teléfono	n	NCMS000	_	3	ATR	_	_
+//	6	inteligente	inteligente	a	AQ0CS0	_	5	MOD	_	_
+//	7	,	,	f	Fc	_	6	punct	_	_
+//	8	fabricado	fabricar	v	VMP00SM	_	5	MOD	_	_
+//	9	por	por	s	SPS00	_	8	BYAG	_	_
+//	10	la	el	d	DA0FS0	_	11	SPEC	_	_
+//	11	compañía	compañía	n	NCFS000	_	9	COMP	_	_
+//	12	canadiense	canadiense	a	AQ0CS0	_	11	MOD	_	_
+//	13	Blackberry	blackberry	n	NP00000	_	11	MOD	_	_
+//	14	.	.	f	Fp	_	13	punct	_	_
 
-	 * 
-	 */
+//	ID:275
+//	property subject: RPG
+//	property subject uri: http://dbpedia.org/resource/IBM_RPG
+//	property object: IBM
+//	property object uri: http://dbpedia.org/resource/IBM
+//	sentence:: 
+//	1	El	el	d	DA0MS0	_	2	SPEC	_	_
+//	2	lenguaje	lenguaje	n	NCMS000	_	6	SUBJ	_	_
+//	3	de	de	s	SPS00	_	2	MOD	_	_
+//	4	programación	programación	n	NCFS000	_	3	COMP	_	_
+//	5	RPG	rpg	n	NP00000	_	2	MOD	_	_
+//	6	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	7	un	uno	d	DI0MS0	_	8	SPEC	_	_
+//	8	lenguaje	lenguaje	n	NCMS000	_	6	ATR	_	_
+//	9	de	de	s	SPS00	_	8	MOD	_	_
+//	10	programación	programación	n	NCFS000	_	9	COMP	_	_
+//	11	desarrollado	desarrollar	v	VMP00SM	_	8	MOD	_	_
+//	12	por	por	s	SPS00	_	11	BYAG	_	_
+//	13	IBM	ibm	n	NP00000	_	12	COMP	_	_
+//	14	en	en	s	SPS00	_	11	MOD	_	_
+//	15	1964	1964	z	Z	_	14	COMP	_	_
+//	16	y	y	c	CC	_	15	COORD	_	_
+//	17	diseñado	diseñar	v	VMP00SM	_	16	CONJ	_	_
+//	18	para	para	s	SPS00	_	17	MOD	_	_
+//	19	generar	generar	v	VMN0000	_	18	COMP	_	_
+//	20	informes	informe	n	NCMP000	_	19	DO	_	_
+//	21	comerciales	comercial	a	AQ0CP0	_	20	MOD	_	_
+//	22	o	o	c	CC	_	21	COORD	_	_
+//	23	de	de	s	SPS00	_	22	CONJ	_	_
+//	24	negocios	negocio	n	NCMP000	_	23	COMP	_	_
+//	25	.	.	f	Fp	_	24	punct	_	_
+//
+//	ID:471
+//	property subject: Superman
+//	property subject uri: http://dbpedia.org/resource/Superman_(Sunsoft_game)
+//	property object: Sunsoft
+//	property object uri: http://dbpedia.org/resource/Sunsoft
+//	sentence:: 
+//	1	Superman	superman	n	NP00000	_	2	SUBJ	_	_
+//	2	es	ser	v	VSIP3S0	_	0	ROOT	_	_
+//	3	un	uno	d	DI0MS0	_	4	SPEC	_	_
+//	4	videojuego	videojuego	n	NCMS000	_	2	ATR	_	_
+//	5	desarrollado	desarrollar	v	VMP00SM	_	4	MOD	_	_
+//	6	y	y	c	CC	_	5	COORD	_	_
+//	7	distribuido	distribuir	v	VMP00SM	_	6	CONJ	_	_
+//	8	por	por	s	SPS00	_	7	BYAG	_	_
+//	9	Sunsoft	sunsoft	n	NP00000	_	8	COMP	_	_
+//	10	para	para	s	SPS00	_	7	MOD	_	_
+//	11	Sega_Mega_Drive	sega_mega_drive	n	NP00000	_	10	COMP	_	_
+//	12	en	en	s	SPS00	_	7	MOD	_	_
+//	13	1992	1992	z	Z	_	12	COMP	_	_
+//	14	.	.	f	Fp	_	13	punct	_	_
+
+
+
+	
         @Override
         public String getQuery() {
-            String query= "SELECT ?lemma ?e1_arg ?e2_arg ?prep WHERE {"
+            String query= "SELECT ?lemma ?form ?e1_arg ?e2_arg ?prep WHERE {"
 
-                            + "?e1 <conll:head> ?estar ."
-                            + "?e1 <conll:deprel> \"SUBJ\"."
+                            
+                            + "?copula <conll:lemma> \"ser\" ."
+                            + "?copula <conll:cpostag> \"v\" ."
 
-                            + "?estar <conll:lemma> \"estar\" ."
-                            + "?estar <conll:postag> ?pos ."
-                            + "FILTER regex(?pos, \"VAII\") ."
-
+							+ "?e1 <conll:head> ?copula ."
+							+ "?e1 <conll:deprel> \"SUBJ\"."
+                            
+					        + "?noun <conll:head> ?copula ."
+					        + "?noun <conll:deprel> \"ATR\". "
+					        + "?noun <conll:cpostag> \"n\". 
+					        
                             + "?participle <conll:lemma> ?lemma ."
-                            + "?participle <conll:head> ?estar ."
-                            + "?participle <conll:deprel> \"ATR\". "
+                            + "?participle <conll:form> ?form ."
+                            + "?participle <conll:head> ?noun ."
+                            + "?participle <conll:deprel> \"MOD\". "
+                            + "?participle <conll:postag> \"VMP00SM\". "
 
                             + "?p <conll:lemma> ?prep ."
                             + "?p <conll:head> ?participle ."
-                            + "?p <conll:deprel> \"OBLC\" ."
-
+                            + "?p <conll:postag> \"SPS00\"."
+                            + "{?p <conll:deprel> \"BYAG\" .} UNION"
+                            + "{?p <conll:deprel> \"MOD\". }"
 
                             + "?e2 <conll:head> ?p ."
                             + "{?e2 <conll:deprel> \"COMP\". } UNION "
                             + "{?e2 <conll:deprel> \"MOD\". }"
-                            //oder MOD?
+          
 
                             + "?e1 <own:senseArg> ?e1_arg. "
                             + "?e2 <own:senseArg> ?e2_arg. "
@@ -99,10 +271,11 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
 				
 		QueryExecution qExec = QueryExecutionFactory.create(getQuery(), model) ;
                 ResultSet rs = qExec.execSelect() ;
-                String adjective = null;
                 String e1_arg = null;
                 String e2_arg = null;
                 String preposition = null;
+                String participle = null;
+                String lemma = null;
 
                 try {
                  while ( rs.hasNext() ) {
@@ -110,10 +283,11 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
 
 
                          try{
-                                 adjective = qs.get("?lemma").toString();
+                                 participle = qs.get("?form").toString();
                                  e1_arg = qs.get("?e1_arg").toString();
                                  e2_arg = qs.get("?e2_arg").toString();	
-                                 preposition = qs.get("?prep").toString();	
+                                 preposition = qs.get("?prep").toString();
+                                 lemma = qs.get("?lemma").toString();             
                           }
 	        	 catch(Exception e){
 	     	    	e.printStackTrace();
@@ -127,8 +301,15 @@ public class SparqlPattern_ES_Predicative_Participle extends SparqlPattern{
     
 		if(adjective!=null && e1_arg!=null && e2_arg!=null && preposition!=null) {
                     Sentence sentence = this.returnSentence(model);
-                    Templates.getAdjective(model, lexicon, sentence, adjective, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
-            }
+                    Templates.getAdjective(model, lexicon, sentence, participle, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
+                                
+                    if (preposition.equals("por"))
+                    {
+                    	// we need the 
+                    	Templates.getTransitive(model, lexicon, sentence, lemma, e1_arg, e2_arg, this.getReference(model), logger, this.getLemmatizer(),Language.ES,ge
+                    }
+		
+		}
 		
 	}
 
--- matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle_Passive.java
+++ matoll/src/main/java/de/citec/sc/matoll/patterns/spanish/SparqlPattern_ES_Predicative_Participle_Passive.java
@@ -21,58 +21,192 @@ public class SparqlPattern_ES_Predicative_Participle_passive extends SparqlPatte
 	Logger logger = LogManager.getLogger(SparqlPattern_ES_Predicative_Participle_passive.class.getName());
 	
 	
-	// should be subsumed by 3 or 4 or 6...
-	
-	// fue + es
-
-// ser o estar
-//está casado con / estuvo casado / ha actuado en...
+//	ID:2
+//	property subject: Sodagreen
+//	property subject uri: http://dbpedia.org/resource/Sodagreen
+//	property object: Sodagreen
+//	property object uri: http://dbpedia.org/resource/Sodagreen
+//	sentence:: 
+//	1	Sodagreen	sodagreen	n	NP00000	_	2	SUBJ	_	_
+//	2	fue	ser	v	VSIS3S0	_	0	ROOT	_	_
+//	3	nombrado	nombrar	v	VMP00SM	_	2	ATR	_	_
+//	4	por	por	s	SPS00	_	3	BYAG	_	_
+//	5	Shih	shih	n	NP00000	_	4	COMP	_	_
+//	6	,	,	f	Fc	_	5	punct	_	_
+//	7	entonces	entonces	r	RG	_	8	MOD	_	_
+//	8	líder	líder	n	NCCS000	_	2	MOD	_	_
+//	9	de	de	s	SPS00	_	8	MOD	_	_
+//	10	la	el	d	DA0FS0	_	11	SPEC	_	_
+//	11	banda	banda	n	NCFS000	_	9	COMP	_	_
+//	12	y	y	c	CC	_	11	COORD	_	_
+//	13	Wu	wu	n	NP00000	_	12	CONJ	_	_
+//	14	,	,	f	Fc	_	13	punct	_	_
+//	15	el	el	d	DA0MS0	_	16	SPEC	_	_
+//	16	vocalista	vocalista	n	NCCS000	_	2	ATR	_	_
+//	17	.	.	f	Fp	_	16	punct	_	_
 //
-//New Parse:
-//1	El	el	d	DA0MS0	_	2	SPEC	_	_
-//2	proyecto	proyecto	n	NCMS000	_	3	SUBJ	_	_
-//3	está	estar	v	VAIP3S0	_	0	ROOT	_	_
-//4	liderado	liderar	v	VMP00SM	_	3	ATR	_	_
-//5	por	por	s	SPS00	_	4	BYAG	_	_
-//6	Theo_de_Raadt	theo_de_raadt	n	NP00000	_	5	COMP	_	_
-//7	,	,	f	Fc	_	6	punct	_	_
-//8	residente	residente	n	NCCS000	_	6	_	_	_
-//9	en	en	s	SPS00	_	8	MOD	_	_
-//10	Calgary	calgary	n	NP00000	_	9	COMP	_	_
-//11	.	.	f	Fp	_	10	punct	_	_
+//	ID:318
+//	property subject: Rage
+//	property subject uri: http://dbpedia.org/resource/Rage_(video_game)
+//	property object: Id Software
+//	property object uri: http://dbpedia.org/resource/Id_Software
+//	sentence:: 
+//	1	"	"	f	Fe	_	4	MOD	_	_
+//	2	Rage	rage	n	NP00000	_	1	MOD	_	_
+//	3	"	"	f	Fe	_	4	SUBJ	_	_
+//	4	fue	ser	v	VSIS3S0	_	0	ROOT	_	_
+//	5	desarrollado	desarrollar	v	VMP00SM	_	4	ATR	_	_
+//	6	por	por	s	SPS00	_	5	BYAG	_	_
+//	7	"	"	f	Fe	_	6	COMP	_	_
+//	8	id	ir	v	VMM02P0	_	9	SPEC	_	_
+//	9	Software	software	n	NP00000	_	4	SUBJ	_	_
+//	10	"	"	f	Fe	_	9	punct	_	_
+//	11	y	y	c	CC	_	9	COORD	_	_
+//	12	distribuido	distribuir	v	VMP00SM	_	11	CONJ	_	_
+//	13	por	por	s	SPS00	_	12	MOD	_	_
+//	14	"	"	f	Fe	_	13	COMP	_	_
+//	15	Bethesda_Softworks	bethesda_softworks	n	NP00000	_	4	MOD	_	_
+//	16	"	"	f	Fe	_	15	punct	_	_
+//	17	.	.	f	Fp	_	15	punct	_	_
+//
 
+//	ID:2
+//	property subject: Sodagreen
+//	property subject uri: http://dbpedia.org/resource/Sodagreen
+//	property object: Sodagreen
+//	property object uri: http://dbpedia.org/resource/Sodagreen
+//	sentence:: 
+//	1	Sodagreen	sodagreen	n	NP00000	_	2	SUBJ	_	_
+//	2	fue	ser	v	VSIS3S0	_	0	ROOT	_	_
+//	3	nombrado	nombrar	v	VMP00SM	_	2	ATR	_	_
+//	4	por	por	s	SPS00	_	3	BYAG	_	_
+//	5	Shih	shih	n	NP00000	_	4	COMP	_	_
+//	6	,	,	f	Fc	_	5	punct	_	_
+//	7	entonces	entonces	r	RG	_	8	MOD	_	_
+//	8	líder	líder	n	NCCS000	_	2	MOD	_	_
+//	9	de	de	s	SPS00	_	8	MOD	_	_
+//	10	la	el	d	DA0FS0	_	11	SPEC	_	_
+//	11	banda	banda	n	NCFS000	_	9	COMP	_	_
+//	12	y	y	c	CC	_	11	COORD	_	_
+//	13	Wu	wu	n	NP00000	_	12	CONJ	_	_
+//	14	,	,	f	Fc	_	13	punct	_	_
+//	15	el	el	d	DA0MS0	_	16	SPEC	_	_
+//	16	vocalista	vocalista	n	NCCS000	_	2	ATR	_	_
+//	17	.	.	f	Fp	_	16	punct	_	_
+
+	
+//	ID:577
+//	property subject: SimCity
+//	property subject uri: http://dbpedia.org/resource/SimCity_(1989_video_game)
+//	property object: Maxis
+//	property object uri: http://dbpedia.org/resource/Maxis
+//	sentence:: 
+//	1	A_el_igual_que	al_igual_que	c	CS	_	0	ROOT	_	_
+//	2	los	el	d	DA0MP0	_	3	SPEC	_	_
+//	3	títulos	título	n	NCMP000	_	11	SUBJ	_	_
+//	4	anteriores	anterior	a	AQ0CP0	_	3	MOD	_	_
+//	5	(	(	f	Fpa	_	6	punct	_	_
+//	6	a_excepción_de	a_excepción_de	s	SPS00	_	3	MOD	_	_
+//	7	SimCity_Societies	simcity_societies	n	NP00000	_	6	COMP	_	_
+//	8	)	)	f	Fpt	_	7	punct	_	_
+//	9	,	,	f	Fc	_	7	punct	_	_
+//	10	SimCity	simcity	n	NP00000	_	7	MOD	_	_
+//	11	será	ser	v	VSIF3S0	_	1	COMP	_	_
+//	12	desarrollado	desarrollar	v	VMP00SM	_	11	ATR	_	_
+//	13	por	por	s	SPS00	_	12	BYAG	_	_
+//	14	Maxis	maxis	n	NP00000	_	13	COMP	_	_
+//	15	,	,	f	Fc	_	14	punct	_	_
+//	16	una	uno	d	DI0FS0	_	17	SPEC	_	_
+//	17	subsidiaria	subsidiaria	n	NCFS000	_	0	ROOT	_	_
+//	18	de	de	s	SPS00	_	17	MOD	_	_
+//	19	Electronic_Arts.	electronic_arts.	n	NP00000	_	18	COMP	_	_
+//	
+	
+//	ID:238
+//	property subject: MS-DOS
+//	property subject uri: http://dbpedia.org/resource/MS-DOS
+//	property object: Microsoft
+//	property object uri: http://dbpedia.org/resource/Microsoft
+//	sentence:: 
+//	1	Aunque	aunque	c	CC	_	0	ROOT	_	_
+//	2	MS-DOS	ms-dos	v	VMP00PM	_	1	_	_	_
+//	3	y	y	c	CC	_	2	COORD	_	_
+//	4	PC-DOS	pc-dos	n	NP00000	_	3	CONJ	_	_
+//	5	fueron	ser	v	VSIS3P0	_	17	MOD	_	_
+//	6	desarrollados	desarrollar	v	VMP00PM	_	5	ATR	_	_
+//	7	por	por	s	SPS00	_	6	BYAG	_	_
+//	8	Microsoft	microsoft	n	NP00000	_	7	COMP	_	_
+//	9	e	y	c	CC	_	8	COORD	_	_
+//	10	IBM	ibm	n	NP00000	_	9	CONJ	_	_
+//	11	en_paralelo	en_paralelo	r	RG	_	6	MOD	_	_
+//	12	,	,	f	Fc	_	11	punct	_	_
+//	13	los	el	d	DA0MP0	_	15	SPEC	_	_
+//	14	dos	2	z	Z	_	15	MOD	_	_
+//	15	productos	producto	n	NCMP000	_	17	SUBJ	_	_
+//	16	se	se	p	P00CN000	_	15	MOD	_	_
+//	17	separaron	separar	v	VMIS3P0	_	0	ROOT	_	_
+//	18	con	con	s	SPS00	_	17	OBLC	_	_
+//	19	el	el	d	DA0MS0	_	20	SPEC	_	_
+//	20	tiempo	tiempo	n	NCMS000	_	18	COMP	_	_
+//	21	.	.	f	Fp	_	20	punct	_	_
+
+//	ID:318
+//	property subject: Rage
+//	property subject uri: http://dbpedia.org/resource/Rage_(video_game)
+//	property object: Id Software
+//	property object uri: http://dbpedia.org/resource/Id_Software
+//	sentence:: 
+//	1	"	"	f	Fe	_	4	MOD	_	_
+//	2	Rage	rage	n	NP00000	_	1	MOD	_	_
+//	3	"	"	f	Fe	_	4	SUBJ	_	_
+//	4	fue	ser	v	VSIS3S0	_	0	ROOT	_	_
+//	5	desarrollado	desarrollar	v	VMP00SM	_	4	ATR	_	_
+//	6	por	por	s	SPS00	_	5	BYAG	_	_
+//	7	"	"	f	Fe	_	6	COMP	_	_
+//	8	id	ir	v	VMM02P0	_	9	SPEC	_	_
+//	9	Software	software	n	NP00000	_	4	SUBJ	_	_
+//	10	"	"	f	Fe	_	9	punct	_	_
+//	11	y	y	c	CC	_	9	COORD	_	_
+//	12	distribuido	distribuir	v	VMP00SM	_	11	CONJ	_	_
+//	13	por	por	s	SPS00	_	12	MOD	_	_
+//	14	"	"	f	Fe	_	13	COMP	_	_
+//	15	Bethesda_Softworks	bethesda_softworks	n	NP00000	_	4	MOD	_	_
+//	16	"	"	f	Fe	_	15	punct	_	_
+//	17	.	.	f	Fp	_	15	punct	_	_
+
+
+	
         @Override
         public String getQuery() {
-            String query = "SELECT ?lemma ?form ?e1_arg ?e2_arg  WHERE {"
+            String query = "SELECT ?lemma ?form ?prep ?e1_arg ?e2_arg  WHERE {"
 
-                            + "?copula <conll:lemma> \"estar\" ."
-                            + "?copula <conll:postag> ?copula_pos ."
-                            + "FILTER regex(?copula_pos, \"VAIP\") ."
+				 + "?auxpass <conll:lemma> \"ser\" ."
+				 + "?auxpass <conll:cpostag> \"v\" ."
+				
+				 + "?e1 <conll:head> ?auxpass ."
+				 + "?e1 <conll:deprel> \"SUBJ\"."
 
+				 + "?participle <conll:lemma> ?lemma ."
+				 + "?participle <conll:form> ?form ."
+				 + "?participle <conll:head> ?auxpass ."
+				 + "?participle <conll:deprel> \"ATR\". "
+				 + "?participle <conll:postag> \"VMP00SM\". "
+				
+				 + "?p <conll:lemma> ?prep ."
+				 + "?p <conll:head> ?participle ."
+				 + "?p <conll:postag> \"SPS00\"."
+				 + "{?p <conll:deprel> \"BYAG\" .} UNION"
+				 + "{?p <conll:deprel> \"MOD\". }"
+				
+				 + "?e2 <conll:head> ?p ."
+				 + "{?e2 <conll:deprel> \"COMP\". } UNION "
+				 + "{?e2 <conll:deprel> \"MOD\". }"
+				
 
-                            + "?participle <conll:postag> ?participle_pos . "
-                            + "FILTER regex(?participle_pos, \"VMP\") ."
-                            + "?participle <conll:form> ?form . "
-                            + "?participle <conll:lemma> ?lemma . "
-                            + "?participle <conll:head> ?copula . "
-                            + "?participle <conll:deprel> \"ATR\" . "
-
-                            + "?e1 <conll:head> ?copula . "
-                            + "?e1 <conll:deprel> \"SUBJ\". "
-
-                            + "?p <conll:head> ?participle . "
-                            + "?p <conll:postag> ?prep_pos . "
-                            + "FILTER regex(?prep_pos, \"SPS\") ."
-                            + "?p <conll:lemma> \"por\" . "
-                            // can be OBLC as well
-                            + "{?p <conll:deprel> \"BYAG\" .} UNION "
-                            + "{?p <conll:deprel> \"OBLC\" .}"
-
-                            + "?e2 <conll:head> ?p . "
-                            + "?e2 <conll:deprel> \"COMP\" . "
-
-                            + "?e1 <own:senseArg> ?e1_arg. "
-                            + "?e2 <own:senseArg> ?e2_arg. "
+				 + "?e1 <own:senseArg> ?e1_arg. "
+				 + "?e2 <own:senseArg> ?e2_arg. "
+            		
+            		
                             + "}";
             return query;
         }
@@ -87,11 +221,11 @@ public class SparqlPattern_ES_Predicative_Participle_passive extends SparqlPatte
 		
 		QueryExecution qExec = QueryExecutionFactory.create(getQuery(), model) ;
                 ResultSet rs = qExec.execSelect() ;
-                String verb = null;
+                String participle = null;
                 String e1_arg = null;
                 String e2_arg = null;
                 String preposition = null;
-                String verb_lemma = null;
+                String lemma = null;
 
                 try {
                  while ( rs.hasNext() ) {
@@ -99,10 +233,12 @@ public class SparqlPattern_ES_Predicative_Participle_passive extends SparqlPatte
 
 
                          try{
-                                 verb = qs.get("?form").toString();
+                                 participle = qs.get("?form").toString();
                                  e1_arg = qs.get("?e1_arg").toString();
                                  e2_arg = qs.get("?e2_arg").toString();	
-                                 verb_lemma = qs.get("?lemma").toString();
+                                 lemma = qs.get("?lemma").toString();
+                                 participle = qs.get("?form").toString();
+                                 preposition = qs.get("?prep").toString();
                           }
 	        	 catch(Exception e){
 	     	    	e.printStackTrace();
@@ -116,12 +252,12 @@ public class SparqlPattern_ES_Predicative_Participle_passive extends SparqlPatte
     
 		if(verb!=null && e1_arg!=null && e2_arg!=null) {
                     Sentence sentence = this.returnSentence(model);
-                    Templates.getAdjective(model, lexicon, sentence, verb, e1_arg, e2_arg, "por", this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
-                    /*
-                    Also create a transitive verb, whoch changed arguments and the lemmatized verb.
-                    */
-                    Templates.getTransitiveVerb(model, lexicon, sentence, verb_lemma, e2_arg, e1_arg, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
-
+                    Templates.getAdjective(model, lexicon, sentence, participle, e1_arg, e2_arg, preposition, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
+     
+                    if (preposition.equals("por"))
+                    {
+                    	Templates.getTransitiveVerb(model, lexicon, sentence, lemma, e2_arg, e1_arg, this.getReference(model), logger, this.getLemmatizer(),Language.ES,getID());
+                    }
                 } 
 		
 	}
