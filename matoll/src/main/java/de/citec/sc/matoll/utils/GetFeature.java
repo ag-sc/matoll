aa2c4affb218d251619c65541c00927121d98630
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2016-05-31 15:38:54 +0200

    removed test files

---
+++ matoll/src/main/java/de/citec/sc/matoll/utils/GetFeature.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.citec.sc.matoll.utils;
+
+import de.citec.sc.matoll.core.LexicalEntry;
+import de.citec.sc.matoll.core.Lexicon;
+
+/**
+ *
+ * @author swalter
+ */
+public class GetFeature {
+    public Feature getFeature(Lexicon lexicon, LexicalEntry entry){
+        Feature feature = new Feature();
+        
+        return feature;
+    }
+}
